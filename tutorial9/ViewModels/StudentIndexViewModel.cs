087f62fd7f4e6a422cf8cc587a1efcfdeed12270
yangjiaqi01 <483248575@qq.com>
2024-11-13 21:20:11 +0800

    Add files via upload

---
+++ tutorial9/ViewModels/StudentIndexViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tutorial7.Models;
+
+namespace tutorial7.ViewModels
+{
+    public class StudentIndexViewModel
+    {
+        public IQueryable<Student> Students { get; set; }
+        public string Search { get; set; }
+        public IEnumerable<CampusWithCount> CampusesWithCount { get; set; }
+        public string Campus { get; set; }
+        public IEnumerable<SelectListItem> CampusFilterItems
+        {
+            get
+            {
+                var allCampuses = CampusesWithCount.Select(cc => new SelectListItem
+                {
+                    Value = cc.CampusName,
+                    Text = cc.CampusNameWithCount
+                });
+                return allCampuses;
+            }
+        }
+    }
+
+    public class CampusWithCount
+    {
+        public int StudentCount { get; set; }
+        public string CampusName { get; set; }
+        public string CampusNameWithCount
+        {
+            get
+            {
+                return CampusName + " (" + StudentCount.ToString() + ")";
+            }
+        }
+    }
+
+}
